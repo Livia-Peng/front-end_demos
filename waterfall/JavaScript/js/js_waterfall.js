d65b3453fdcc66e0bfaaebe75c2f4e82756cdbe7
Livia Peng <2356426722@qq.com>
2017-03-23 17:02:47 +0800

    更新

--- waterfall/JavaScript/js/js_waterfall.js
+++ waterfall/JavaScript/js/js_waterfall.js
@@ -17,7 +17,7 @@ window.onload = function(){
             oBox.appendChild(oPic);
 
             var oImg = document.createElement('img');
-            oImg.src = './images/' + Math.round( Math.random()*20 ) + '.jpg';
+            oImg.src = './images/' + parseInt(Math.random()*10 +Math.random()*10) + '.jpg';
             oPic.appendChild(oImg);
 
             waterfall();
