569e0e0b09ee017ab0639dd953b7152d35921e6b
Livia Peng <2356426722@qq.com>
2017-04-11 17:11:58 +0800

    轻量级计算器

--- calculate/js/script.js
+++ calculate/js/script.js
@@ -4,7 +4,6 @@
 window.onload = function(){
     var screen0 = document.getElementById('screen0'),   //获取上显示器内容
         screen1 = document.getElementById('screen1');   //获取下显示器内容
-
     screen0.value = null;
     screen1.value = null;
     calculate(screen0, screen1);
@@ -35,58 +34,48 @@ function calculate(screen0,screen1){
                     screen1.value = screen1.value.slice(0,-1);
                     count--;
                 }
-            }else if(symbol == '×'){
-                screen1.value += '*';
-            }else if(symbol == '÷'){
-                screen1.value += '/';
             }else{
                 screen1.value += symbol;
             }
         }
         //计算结果
         else if(symbol == '='){
-            if(!screen1.value){
-                return null;
-            }else{
-                screen0.value = screen1.value;
-                screen1.value = count(screen1.value);
-            }
+            screen0.value = screen1.value;
+            screen1.value = test(screen1.value);
         }
     }
 }
 
-function count(text) {
-    //首先寻找最右左括号
-    var index1 = text.lastIndexOf("(");
-    if(index > -1) {
-        //寻找同组右括号
-        var endIndex = text.indexOf(")", index1);
+function test(text) {
+    var index = 0;   //记录符号索引
+
+        //首先计算括号内内容
+    if(text.lastIndexOf("(") > -1){
+        index = text.lastIndexOf("(");
+        var endIndex = text.indexOf(")", index);
         if(endIndex > -1) {
-            //算出括号中的结果
-            var result = count(text.substring(index1 + 1, endIndex));
-            //继续计算表达式
-            text = count(text.substring(0, index1) + result + text.substring(endIndex + 1));
+            var result = test(text.substring(index + 1, endIndex));
+            return test(text.substring(0, index) + result + text.substring(endIndex + 1));
         }
+
+    }else if(text.indexOf("+") >-1){
+        index = text.indexOf("+");
+        return test(text.substring(0, index)) + test(text.substring(index + 1));
+
+    }else if(text.lastIndexOf("-") > -1){
+        index = text.lastIndexOf("-");
+        return test(text.substring(0, index)) - test(text.substring(index + 1));
+
+    }else if(text.indexOf("*") > -1){
+        index = text.indexOf("*");
+        return test(text.substring(0, index)) * test(text.substring(index + 1));
+
+    }else if(text.lastIndexOf("/") > -1){
+        index = text.lastIndexOf("/");
+        return test(text.substring(0, index)) / test(text.substring(index + 1));
+
     }else{
-        var index2 = text.indexOf("+");
-        if (index2 > -1) {
-            text = count(text.substring(0, index2)) + count(text.substring(index2 + 1));
-        }else{
-            var index3 = text.lastIndexOf("-");
-            if (index3 > -1) {
-                text = count(text.substring(0, index3)) -count(text.substring(index3 + 1));
-            }else{
-                var index4 = text.lastIndexOf("*");
-                if (index4 > -1) {
-                    text = count(text.substring(0, index4)) * count(text.substring(index4 + 1));
-                }else{
-                    var index5 = text.lastIndexOf("/");
-                    if (index5 > -1) {
-                        text = count(text.substring(0, index5)) / count(text.substring(index5 + 1));
-                    }
-                }
-            }
-        }
+        return Number(text);
     }
-    return text;
 }
+
